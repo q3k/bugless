9080787cb6ce7e315d82b65d1db5ffc0d5f53e94
Sergiusz Bazanski <q3k@q3k.org>
2019-04-21 01:38:13 +0200

    proto/svc: model: rename GetIssue to GetIssues, implement in dummy

--- svc/model/dummy/dummy.go
+++ svc/model/dummy/dummy.go
@@ -4,9 +4,19 @@ import (
 	"flag"
 
 	"github.com/q3k/bugless/lib/micro"
+	spb "github.com/q3k/bugless/proto/svc"
 
 	log "github.com/inconshreveable/log15"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type service struct {
+}
+
+func (s *service) GetIssues(req *spb.ModelGetIssueRequest, srv spb.Model_GetIssuesServer) error {
+	return status.Error(codes.Unimplemented, "unimplemented in dummy service")
+}
 
 func main() {
 	flag.Parse()
@@ -17,6 +27,9 @@ func main() {
 		l.Crit("could not listen", "err", err)
 		return
 	}
+
+	s := &service{}
+	spb.RegisterModelServer(m.GRPC(), s)
 
 	if err := m.Serve(); err != nil {
 		l.Crit("could not serve", "err", err)
