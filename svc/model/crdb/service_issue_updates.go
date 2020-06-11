d7fa0deda03416b9095dc24086104e6337131d2e
Sergiusz Bazanski <q3k@q3k.org>
2020-06-11 14:53:18 +0200

    model/crdb: move UpdateIssue to service_issue_updates.go

--- svc/model/crdb/service_issue_updates.go
+++ svc/model/crdb/service_issue_updates.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+
 	spb "github.com/q3k/bugless/proto/svc"
 	"github.com/q3k/bugless/svc/model/common/pagination"
 	"github.com/q3k/bugless/svc/model/crdb/db"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *service) GetIssueUpdates(req *spb.ModelGetIssueUpdatesRequest, srv spb.Model_GetIssueUpdatesServer) error {
@@ -30,4 +34,49 @@ func (s *service) GetIssueUpdates(req *spb.ModelGetIssueUpdatesRequest, srv spb.
 		}
 		return len(updates), start, srv.Send(chunk)
 	})
+}
+
+func (s *service) UpdateIssue(ctx context.Context, req *spb.ModelUpdateIssueRequest) (*spb.ModelUpdateIssueResponse, error) {
+	if err := validateUser(req.Author); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "author: %v", err)
+	}
+	if req.New == nil {
+		return nil, status.Error(codes.InvalidArgument, "new state must be set")
+	}
+
+	update := &db.IssueUpdate{
+		IssueID: req.Id,
+		Author:  req.Author.Id,
+	}
+	if req.Comment != "" {
+		update.Comment.Valid = true
+		update.Comment.String = req.Comment
+	}
+	if req.New.Title != "" {
+		update.Title.Valid = true
+		update.Title.String = req.New.Title
+	}
+	if req.New.Assignee != nil {
+		update.Assignee.Valid = true
+		update.Assignee.String = req.New.Assignee.Id
+	}
+	if validateIssueType(req.New.Type) == nil {
+		update.Type.Valid = true
+		update.Type.Int64 = int64(req.New.Type)
+	}
+	// TODO(q3k): fix not being able to set P0, this requires a schema fix
+	if req.New.Priority > 0 && validateIssuePriority(req.New.Priority) == nil {
+		update.Priority.Valid = true
+		update.Priority.Int64 = req.New.Priority
+	}
+	if validateIssueStatus(req.New.Status) == nil {
+		update.Status.Valid = true
+		update.Status.Int64 = int64(req.New.Status)
+	}
+
+	err := s.db.Do(ctx).Issue().Update(update)
+	if err != nil {
+		return nil, err
+	}
+	return &spb.ModelUpdateIssueResponse{}, nil
 }
